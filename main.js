d01c640784162ab9dae9bc8c4cad7050a87e8481
munseok <journy002@naver.com>
2020-10-28 19:46:50 +0900

    portfolio.update

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ workBtnContainer.addEventListener('click', (e) => {
     active.classList.remove('selected');
     const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
     target.classList.add('selected');
+    // ---------------------------------------------------------------
 
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
